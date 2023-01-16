01883df55f7c402a7a1c9a701bbb15e1457d025a
DevAkky <developerakky@gmaiil.com>
2023-01-16 13:48:36 +0530

    install axios

--- src/Ecommerce App/EcomApp.js
+++ src/Ecommerce App/EcomApp.js
@@ -10,6 +10,29 @@ const EcomApp = () => {
       setShowSignUp(!showSignUp)
   }
 
+  let loginFunc = () =>{
+    
+  }
+
+
+  let signupFunc = () =>{
+    const mail = document.getElementById("email");
+    const fname = document.getElementById("fullname")
+    const sUsername = document.getElementById("sUsername")
+    const sPass = document.getElementById("sPass")
+
+    const sData = {
+        mail : mail.value,
+        fname : fname.value,
+        sUsername : sUsername.value,
+        sPass : sPass.value
+    }
+     
+    
+
+  }
+
+
   return (
      <>
      <div className='loginpage'>
@@ -22,46 +45,57 @@ const EcomApp = () => {
                       !showSignUp ? (  <div className='login-wrapper'>
                           <h3 className='title text-center'>Log In</h3>
                           <div className='input-group'>
-                             <input type="text" className='form-control' placeholder='Username or email' id='username'/>
+                             <input type="text" className='form-control' placeholder='Username or email'
+                              id='username'/>
                           </div>
                           <div className='input-group'>
-                             <input type="password" className='form-control' placeholder='Password' id='pass'/>
+                             <input type="password" className='form-control' placeholder='Password'
+                              id='pass'/>
                           </div>
                           <div className='input-group'>
-                             <button type="submit" className='form-control btn btn-primary' placeholder='Enter your password' id='username'>Log In</button>
+                             <button type="submit" className='form-control btn btn-primary' 
+                             placeholder='Enter your password' id='username' onClick={loginFunc}>Log In</button>
                           </div>
                         <a href="#" className='forgot-btn text-center text-primary'>Forgot password?</a>
                          
                           <hr></hr>
 
                          <div className='signup'>
-                          Don't have an account? &nbsp; <span className='text-info' onClick={toggleSignup}>Sign Up</span>
+                          Don't have an account? &nbsp; <span className='text-info'
+                           onClick={toggleSignup}>Sign Up</span>
                            </div>
+
                         <div className='error-msg text-danger text-center'></div>
+
                       </div>)  : 
                       
-                      (  <div className='
-                      '>
+                      (  <div className='signup-wrapper'>
                           <h3 className='title text-center'>Sign Up</h3>
                           <div className='input-group'>
-                             <input type="text" className='form-control' placeholder='Email or Number' id='email'/>
+                             <input type="text" className='form-control' placeholder='Email or Number' 
+                             id='email'/>
                           </div>
                           <div className='input-group'>
-                             <input type="text" className='form-control' placeholder='Full Name' id='fullname'/>
+                             <input type="text" className='form-control' placeholder='Full Name' 
+                             id='fullname'/>
                           </div>
                           <div className='input-group'>
-                             <input type="text" className='form-control' placeholder='Username' id='username'/>
+                             <input type="text" className='form-control' placeholder='Username'
+                              id='sUsername'/>
                           </div>
                           <div className='input-group'>
-                             <input type="password" className='form-control' placeholder='Password' id='pass'/>
+                             <input type="password" className='form-control' placeholder='Password' 
+                             id='sPass'/>
                           </div>
 
                           <div className='input-group'>
-                             <button type="submit" className='form-control btn btn-primary' placeholder='Enter your password' id='username'>Sign Up</button>
+                             <button type="submit" className='form-control btn btn-primary' 
+                             placeholder='Enter your password' id='username' onClick={signupFunc}>Sign Up</button>
                           </div>
                                <hr></hr>
                         <div className='login'>
-                        Have an account? &nbsp; <span className='text-info' onClick={toggleSignup}>Log In</span>
+                        Have an account? &nbsp; <span className='text-info' 
+                        onClick={toggleSignup}>Log In</span>
                            </div>
                         <div className='error-msg text-danger text-center'></div>
                       </div>) 
